2e2e5b6c302119b3e8d8dcd088f9b1f05f0a3864
poojalatthe@yahoo.in <poojalatthe@yahoo.in>
2015-12-11 19:27:05 +0530

    Made Changes to place order

--- OmEnggWebsite/web/js/cssSupport.js
+++ OmEnggWebsite/web/js/cssSupport.js
@@ -1,20 +1,21 @@
 /**
  * Created by om on 26-Nov-15.
  */
+
 function openDropdown(clicked_id) {
 
-    if(document.getElementById(clicked_id).className === "dropdown open")
+    if (document.getElementById(clicked_id).className === "dropdown open")
         document.getElementById(clicked_id).className = "dropdown ";
     else
         document.getElementById(clicked_id).className = "dropdown open";
 }
 
-function load_menu(){
+function load_menu() {
     console.log("hello");
     var link = document.querySelector('link[rel="import"]');
     var content = link.import;
     if (supportsImports()) {
-       console.log("Supports");
+        console.log("Supports");
     } else {
         console.log("no Support");
     }
@@ -43,7 +44,7 @@ function load_map() {
     var marker = new google.maps.Marker({
         position: position,
         map: map,
-        title:"This is the place."
+        title: "This is the place."
     });
 
     var contentString = 'Hello';
@@ -51,8 +52,39 @@ function load_map() {
         content: contentString
     });
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
     });
 
+}
+
+function orderMore() {
+    console.log("Hello");
+    var original = document.getElementById('repeat');
+    var divele = document.createElement("div");
+    divele.innerHTML = createFormField();
+    original.appendChild(divele);
+}
+
+var global =0;
+
+function createFormField() {
+    console.log(global);
+    var attach = '<label class="col-lg-2 control-label text_style">Model Number</label>' +
+        '<div class="col-lg-3">' +
+        '<select class="form-control" name="modelType">' +
+        '<option>NRV-6 - S1</option>  <option>NRV-6 - S2</option>  <option>NRV-6 - S3</option>  <option>NRV-6 - S4</option>' +
+        '<option>NRV-8 - S1</option>  <option>NRV-8 - S2</option> <option>NRV-8 - S3</option>  <option>NRV-8 - S4</option>' +
+        '<option>NRV-10 - S1</option>  <option>NRV-10 - S2</option> <option>NRV-10 - S3</option> <option>NRV-10 - S4</option>' +
+        '<option>NRV-15 - S1</option> <option>NRV-15 - S2</option> <option>NRV-15 - S3</option> <option>NRV-15 - S4</option>' +
+        '<option>NRV-20 - S1</option>      <option>NRV-20 - S2</option>       <option>NRV-20 - S3</option> <option>NRV-20 - S4</option>' +
+        '<option>NRV-25 - S1</option>  <option>NRV-25 - S2</option>    <option>NRV-25 - S3</option>   <option>NRV-25 - S4</option>' +
+        '<option>NRV-30 - S1</option>  <option>NRV-30 - S2</option>  <option>NRV-30 - S3</option>   <option>NRV-30 - S4</option>' +
+        '</select>  </div>  <label class="col-lg-2 control-label text_style">Quantity</label> ' +
+        '<div class="col-lg-3">' +
+        '<input type="number" min="1" max="500" value="1" name="qty"class="form-control">'
+        + '</div>' + '<button class="btn btn-default" type="button" onclick="orderMore();">Order More</button>';
+
+    global ++;
+    return attach;
 }
